0618df1333620a20bc55eca29853adcbbe39b85e
ecrmnn <danieleckermann@gmail.com>
2019-02-15 23:18:18 +0100

    Finishes TypeScript rewrite

--- src/index.ts
+++ src/index.ts
@@ -24,36 +24,42 @@ const trendingGitHub = function (period?: string, language?: string) {
       language = '';
     }
 
+    const array: string[] = [];
+
+    for (let index = 0; index < array.length; index += 1) {
+      const element = array[index];
+    }
+
     return axios.get('https://github.com/trending/' + encodeURIComponent(language) + '?since=' + period)
-      .then(response => {
+      .then((response) => {
         const $ = cheerio.load(response.data);
         const repos: Repository[] = [];
 
         $('li', 'ol.repo-list').each((index, repo) => {
           const title = $(repo).find('h3').text().trim();
 
-          const starLink = '/' + title.replace(/ /g, '') + '/stargazers';
-          const forkLink = '/' + title.replace(/ /g, '') + '/network';
+          const starLink = `/${title.replace(/ /g, '')}/stargazers`;
+          const forkLink = `/${title.replace(/ /g, '')}/network`;
 
           repos.push({
             author: title.split(' / ')[0],
             name: title.split(' / ')[1],
-            href: 'https://github.com/' + title.replace(/ /g, ''),
+            href: `https://github.com/${title.replace(/ /g, '')}`,
             description: $(repo).find('p .py-1').text().trim() || null,
             language: $(repo).find('[itemprop=programmingLanguage]').text().trim(),
             stars: parseInt($(repo).find('[href="' + starLink + '"]').text().trim().replace(',', '') || '0'),
             forks: parseInt($(repo).find('[href="' + forkLink + '"]').text().trim().replace(',', '') || '0'),
-            starsToday: parseInt($(repo).find('span.float-sm-right:contains("stars today")').text().trim().replace('stars today', '').replace(',', '') || '0')
+            starsToday: parseInt($(repo).find('span.float-sm-right:contains("stars today")').text().trim().replace('stars today', '').replace(',', '') || '0'),
           });
         });
 
         resolve(repos);
       })
-      .catch(err => {
+      .catch((err) => {
         reject(err);
       });
   });
-}
+};
 
 export default trendingGitHub;
 
